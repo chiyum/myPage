ad2fbae225d9ec1e4156ec40c77f6dd144d19390
pray870712 <pray870712@gmail.com>
2021-08-02 22:17:14 +0800

    beta 1

---
+++ model/model.js
@@ -0,0 +1,31 @@
+let navButton = document.querySelector('.navclick');
+let nav =document.querySelector('.nav');
+let navUl = document.querySelector('.nav ul');
+let navoff = document.querySelector('.nav p')
+let wrap = document.querySelector('.wrap');//外框
+
+
+wrap.style.height = innerHeight+"px";
+window.onresize =function(){
+    wrap.style.height = innerHeight+"px";
+};
+//選單高度自適應
+navUl.style.height = innerHeight+"px";
+window.onresize =function(){
+    navUl.style.height = innerHeight+"px";
+};
+
+// wrap.classList.add("wrapbg") nav展開改變
+
+navButton.onclick=function(){ //選單展開
+    nav.style.left =0;
+    navoff.classList.remove("hide");
+    setTimeout("nav.classList.add('bgmed')",350);
+    
+};
+
+navoff.onclick=function(){//選單收回
+    nav.style.left =-100+"%";
+    navoff.classList.add("hide");
+    nav.classList.remove("bgmed");
+}
